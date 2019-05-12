c26580e649ce99f78e7d5bffccc3ad5b3d0c5c9d
Rodney Marable <rodney.marable@gmail.com>
2019-05-11 22:03:29 -0400

    Added a template in the HPC performance tree for submitting the default Slurm scripts.
    Fixed an issue with the Slurm sbatch generation script that was
    preventing cluster builds.

--- ClusterMaker/performance/perf-qsub.sh
+++ ClusterMaker/performance/perf-qsub.sh
@@ -2,13 +2,13 @@
 # Name:		perf-qsub.sh
 # Author:	Rodney Marable <rodney.marable@gmail.com>
 # Created On:	February 14, 2018
-# Last Changed:	March 13, 2018
+# Last Changed:	May 11, 2019
 # Purpose:	Wrapper for submitting multiple qsub-Axb_random cluster jobs
 ################################################################################
 
 #!/bin/sh
 
-for file in `ls qsub-Axb_random.[0-9]* | sort -k2,2n -t.`
+for qsubfile in `ls qsub-Axb_random.[0-9]* | sort -k2,2n -t.`
 do
-	qsub $file
+	qsub $qsubfile
 done
--- ClusterMaker/performance/perf-sbatch.sh
+++ ClusterMaker/performance/perf-sbatch.sh
@@ -2,13 +2,13 @@
 # Name:		perf-sbatch.sh
 # Author:	Rodney Marable <rodney.marable@gmail.com>
 # Created On:	May 8, 2019
-# Last Changed:	May 8m 2019
+# Last Changed:	May 11, 2019
 # Purpose:	Wrapper for submitting multiple sbatch-Axb_random cluster jobs
 ################################################################################
 
 #!/bin/sh
 
-for file in `ls sbatch-Axb_random.[0-9]* | sort -k2,2n -t.`
+for sbatchfile in `ls sbatch-Axb_random.[0-9]* | sort -k2,2n -t.`
 do
-	sbatch $file
+	sbatch $sbatchfile
 done
